f38d6c1b6b3603c68ca4567e47c49e5d67ad7b6e
Santhosh-7603 <santhoshm7603@gmail.com>
2025-09-12 21:09:59 +0530

    summa

--- frontend/src/helpers/AxiosInstance.jsx
+++ frontend/src/helpers/AxiosInstance.jsx
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-// Environment variables from Vite
-const backendUrlGlobal = import.meta.env.VITE_BACKEND_URL_GLOBAL ||  import.meta.env.VITE_BACKEND_URL_LOCAL
-const backendUrlLocal = import.meta.env.VITE_BACKEND_URL_LOCAL;
-const appEnvironment = import.meta.env.VITE_APP_ENVIRONMENT;
-const appUrl = appEnvironment === "production" ? backendUrlGlobal : backendUrlLocal 
-// Create Axios instance
+const url = import.meta.env.VITE_BACKEND_URL
 const axiosInstance = axios.create({
-  baseURL: appUrl+"/api",
+  baseURL: `${url}/api`,
   withCredentials: true,
 });
 
